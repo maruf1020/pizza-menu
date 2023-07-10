846f5dceda5786612100ea2c287dacdb105c755b
maruf1020 <marufbillah03033@gmail.com>
2023-07-11 01:10:39 +0600

    complete lecture 40

--- src/index.js
+++ src/index.js
@@ -1,8 +1,102 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+
 function App() {
-    return <h1>Hello MF React</h1>
+    return (
+        <>
+            <Header />
+            <Menu />
+            <Footer />
+
+        </>
+
+    )
+}
+
+function Header() {
+    return <h1>Int. Dhakaiya Pizza</h1>
+}
+
+function Menu() {
+    return (
+        <div>
+            <h2>Menu</h2>
+            <Pizza />
+        </div>
+    )
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 0;
+    const closeHour = 23;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    // if (isOpen) {
+    //     alert("We're currently open");
+    // } else {
+    //     alert("we are currently Close")
+    // }
+
+    return <footer> {new Date().toLocaleTimeString()} We're currently open</footer>
+    // return React.createElement('footer', null, "we're currently open")
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="pizza" />
+            <h2>Pizza spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    )
 }
 
 const rootElm = document.getElementById('root');
